4055df42f6c6d5d452cd0676c9df0ba4ebec051a
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-03 19:55:46 +0530

    58136

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-class OrderStatus(models.Model):
-    name= models.CharField(max_length=50, unique=True)
+class Coupon(models.Model):
+    code= models.CharField(max_length=50, unique=True)
+    discount= models.DecimalField(max_digit=5,decimal_place=2)
+    is_active= models.BooleanField(default=True)
+    valid_from = models.DateTimeField()
+    valid_until = models.DateTimeField()
     def __str__(self):
-        return self.name
+        return self.code
         
 class Order(models.Model):
     customer_name= models.CharField(max_length=100)
