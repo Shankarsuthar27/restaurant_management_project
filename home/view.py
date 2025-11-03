dcc5c5b467d6d0c15b21515fa002a66f36e1223a
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-03 20:50:56 +0530

    58137

--- home/view.py
+++ home/view.py
@@ -1,3 +1,3 @@
-from rest_framework import generics
-from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+class MenuCategoryListView(generics.ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serializer_class = MenuCategorySerializer
