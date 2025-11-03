629e63226582414e439aa07e94753817291c805a
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-03 20:55:57 +0530

    58137

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Coupon
+from .models import MenuCategory
+
+class MenuCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuCategory
+        field = ['id','name']
 
 class CouponSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coupon
         field = ['code','discount','is_active','valid_from', 'valid_until']
+
