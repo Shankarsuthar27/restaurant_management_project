c630bc1cb702f513b12ab86b472bee790c640aa8
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-05 10:33:53 +0530

    58350

--- home/models.py
+++ home/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 class MenuCategory(models.Model):
     name= models.CharField(max_length=100, unique=True)
+    address= models.TextField()
+    has_delivery = models.BooleanField(default=False)
     def __str__(self):
         return self.name
 # Create your models here.
