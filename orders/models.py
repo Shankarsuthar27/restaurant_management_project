36c0b6ba531848183aa6de1389b32e673c9af019
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-05 20:30:04 +0530

    58352

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 class Coupon(models.Model):
     code= models.CharField(max_length=50, unique=True)
     discount= models.DecimalField(max_digit=5,decimal_place=2)
@@ -9,9 +10,11 @@ class Coupon(models.Model):
         return self.code
         
 class Order(models.Model):
-    customer_name= models.CharField(max_length=100)
+    customer_name= models.ForeignKey(User,on_delete=models.CASCADE, related_name= 'order')
     total_price= models.DecimalField(max_length=10, decimal_place=2)
     status= models.ForeignKey(OrderStatus, on_delete = models.SET_NULL, null=True) 
+    coupon= models.ManyToManyField(Coupon)
+
 
     def __str__(self):
         return f"Order #{self.id}- {self.customer_name}"       
