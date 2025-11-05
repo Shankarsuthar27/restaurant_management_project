d5c34aa6374dbe1f6ece2674b2830d79f8930f78
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-05 10:36:02 +0530

    58350

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
+from your_app.models import Restaurant
+Restaurant.objects.create(name='Text Cafe', address='Main Street', has_delivery=True)
 
-# Register your models here.
