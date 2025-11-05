417ae0ebcb3a12b719d98df0dfe3192d92a4f760
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-05 20:46:55 +0530

    58353

--- home/views.py
+++ home/views.py
@@ -1,2 +1,7 @@
-
+from rest_framework import generics
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
+class FeaturedItemView(generics.ListAPIView):
+    queryset = MenuCategory.objects.filter(is_featured=True)
+    serializer_class = MenuCategorySerializer
 
