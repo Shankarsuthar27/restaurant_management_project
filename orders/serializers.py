ebc600a8a4eccc96dcf200796c5953f563c463d0
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-05 20:18:50 +0530

    58352

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,2 +1,14 @@
 from rest_framework import serializers
 from .models import Order,Coupon
+
+class CouponSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coupon
+        fields = ['id','name','price']
+
+class OrderSerializer(serializers.ModelSerializer):
+    coupon= CouponSerializer(many=True)
+
+    class Meta:
+        model=Order
+        fields=['id','date','total_price','items']
