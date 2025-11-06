364d6ec2f4eee1e82fc750a4ea71dffbdff34b56
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-06 20:15:01 +0530

    58530

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 class Coupon(models.Model):
     code= models.CharField(max_length=50, unique=True)
     discount= models.DecimalField(max_digit=5,decimal_place=2)
