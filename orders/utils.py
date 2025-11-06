1228e6beccbf96cd9dcf0e4e75cf944b3dd9586d
Shankarsuthar27 <ss2137789@gmail.com>
2025-11-06 19:42:42 +0530

    58526

--- orders/utils.py
+++ orders/utils.py
@@ -1,9 +1,16 @@
 import string
 import secrets
 from .models import Coupon
+from django.db.models import Sum
+from .models import Order
 
 def generate_coupon_code(length=10):
+
     characters = string.ascii_uppercase + string.digits
+
+    orders = Order.objects.filter(created_at__date=date)
+    total = orders.aggregate(total_sum = Sum('total_price')) ['total_sum']
+ 
 
     while True:
         code = ''.join(secrets.choice(characters) for_in range(length))
